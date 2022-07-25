f7e588e6f095a75cdcf818ec26a602c861207f40
sunofbeat <sunmam964@naver.com>
2022-07-25 17:54:42 +0900

    mybatis

---
+++ src/ch03/ex01/Main.java
@@ -0,0 +1,12 @@
+package ch03.ex01;
+
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		mapper.selectUsers().forEach(System.out::println);
+	}
+
+}
